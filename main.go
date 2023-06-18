6ff49ed8a1ffd246ba4fea7d7d00939803081534
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-18 13:48:35 -0700

    wip: endpoint and functions

--- main.go
+++ main.go
@@ -1,45 +1,70 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"speclang/ast"
 	"speclang/lexer"
 	"speclang/parser"
 )
 
 func main() {
-	lex := lexer.New(input)
+	input := `
+	@uri("/products")
+	endpoint Product {
+		@uri("/")
+		get AllProducts() {
+			// Retrieve all products
+		}
 
-	p := parser.New(lex)
+		@uri("/{id}")
+		get ProductById(id int) {
+			// Retrieve product by ID
+		}
+
+		@uri("/")
+		post addProduct(product ProductInput) {
+			// Add a new product
+		}
+
+		@uri("/{id}")
+		patch updateProductById(id int, product ProductInput) {
+			// Update product by ID
+		}
+
+		@uri("/{id}")
+		delete ProductById(id int) {
+			// Delete product by ID
+		}
+	}
+	`
+
+	l := lexer.New(input)
+	p := parser.New(l)
 	program := p.Parse()
 
+	var currentEndpointURI string
+	var currentFunctionURI string
 	for _, stmt := range program.Statements {
-		println(stmt.String())
-	}
-
-}
-
-func printParserErrors(out io.Writer, errors []string) {
-	for _, msg := range errors {
-
-		_, _ = io.WriteString(out, "\t"+msg+"\n")
-	}
-}
-
-const input = `@uri("http://example.com/thing")
-			  endpoint Product {
-				@uri("/")
-				get allProducts() {
-					# Define the query for retrieving all products
-				}
-			
-				@uri("/{id}")
-				post product() {
-                   # Define the query for retrieving a single product
-				}
-				
+		switch stmt := stmt.(type) {
+		case *ast.AnnotationStatement:
+			if stmt.Name.Value == "uri" {
+				currentEndpointURI = stmt.Value.(*ast.StringLiteral).Value
 			}
-			type Product {
-				name string "binding:json, required"
-				description string "binding:json, required"
-				price float64 "binding:json, required"
-			}`
+		case *ast.EndpointStatement:
+			endpointURI := currentEndpointURI
+			fmt.Printf("Endpoint: %s\n", endpointURI)
+			fmt.Printf("Endpoint: %s\n", stmt.URI.Value)
+			for _, innerStmt := range stmt.Block.Statements {
+
+				switch innerStmt := innerStmt.(type) {
+				case *ast.AnnotationStatement:
+					if innerStmt.Name.Value == "uri" {
+						currentFunctionURI = innerStmt.Value.(*ast.StringLiteral).Value
+					}
+				case *ast.FunctionStatement:
+					fmt.Printf("METHOD: %s, FUNCTION: %s, URI: %s%s\n", innerStmt.Token.Literal, innerStmt.Name.Value, endpointURI, currentFunctionURI)
+				}
+			}
+		}
+	}
+}
