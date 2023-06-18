9d726a541baecc1bdfd31c4ea51dcd5e261bfa5d
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-18 14:05:39 -0700

    wip: endpoint and functions

--- main.go
+++ main.go
@@ -2,48 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"speclang/ast"
 	"speclang/lexer"
 	"speclang/parser"
+	"strings"
 )
 
 func main() {
-	input := `
-	@uri("/products")
-	endpoint Product {
-		@uri("/")
-		get AllProducts() {
-			// Retrieve all products
-		}
-
-		@uri("/{id}")
-		get ProductById(id int) {
-			// Retrieve product by ID
-		}
-
-		@uri("/")
-		post addProduct(product ProductInput) {
-			// Add a new product
-		}
-
-		@uri("/{id}")
-		patch updateProductById(id int, product ProductInput) {
-			// Update product by ID
-		}
-
-		@uri("/{id}")
-		delete ProductById(id int) {
-			// Delete product by ID
-		}
+	// Read the speclang file
+	filePath := "product.speclang"
+	input, err := ReadSpeclangFile(filePath)
+	if err != nil {
+		log.Fatal(err)
 	}
-	`
 
+	// Parse the speclang input
+	program := ParseSpeclang(input)
+
+	// Generate Swagger specification
+	swagger := GenerateSwaggerSpec(program)
+
+	// Write Swagger specification to a file
+	outputPath := "spec.yaml"
+	err = WriteSwaggerSpec(outputPath, swagger)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Swagger specification written to %s\n", outputPath)
+
+}
+
+// ReadSpeclangFile reads the content of a speclang file.
+func ReadSpeclangFile(filePath string) (string, error) {
+	input, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(input), nil
+}
+
+// ParseSpeclang parses the speclang input and returns the program AST.
+func ParseSpeclang(input string) *ast.Program {
 	l := lexer.New(input)
 	p := parser.New(l)
-	program := p.Parse()
+	return p.Parse()
+}
+
+// GenerateSwaggerSpec generates the Swagger specification from the program AST.
+func GenerateSwaggerSpec(program *ast.Program) string {
+	var sb strings.Builder
+	sb.WriteString("swagger: '2.0'\n")
+	sb.WriteString("info:\n")
+	sb.WriteString("  version: 1.0.0\n")
+	sb.WriteString("  title: My API\n")
+	sb.WriteString("paths:\n")
 
 	var currentEndpointURI string
-	var currentFunctionURI string
 	for _, stmt := range program.Statements {
 		switch stmt := stmt.(type) {
 		case *ast.AnnotationStatement:
@@ -52,19 +70,32 @@ func main() {
 			}
 		case *ast.EndpointStatement:
 			endpointURI := currentEndpointURI
-			fmt.Printf("Endpoint: %s\n", endpointURI)
-			fmt.Printf("Endpoint: %s\n", stmt.URI.Value)
+			sb.WriteString("  " + endpointURI + ":\n")
 			for _, innerStmt := range stmt.Block.Statements {
-
 				switch innerStmt := innerStmt.(type) {
 				case *ast.AnnotationStatement:
 					if innerStmt.Name.Value == "uri" {
-						currentFunctionURI = innerStmt.Value.(*ast.StringLiteral).Value
+						// Store the function URI if needed for further processing
+						// currentFunctionURI = innerStmt.Value.(*ast.StringLiteral).Value
 					}
 				case *ast.FunctionStatement:
-					fmt.Printf("METHOD: %s, FUNCTION: %s, URI: %s%s\n", innerStmt.Token.Literal, innerStmt.Name.Value, endpointURI, currentFunctionURI)
+					sb.WriteString("    " + strings.ToLower(innerStmt.Token.Literal) + ":\n")
+					sb.WriteString("      summary: " + innerStmt.Name.Value + "\n")
+					sb.WriteString("      operationId: " + innerStmt.Name.Value + "\n")
+					sb.WriteString("      parameters:\n")
+					// Add parameters here if needed
+					sb.WriteString("      responses:\n")
+					sb.WriteString("        200:\n")
+					sb.WriteString("          description: Success\n")
 				}
 			}
 		}
 	}
+
+	return sb.String()
+}
+
+// WriteSwaggerSpec writes the Swagger specification to a file.
+func WriteSwaggerSpec(outputPath string, swaggerSpec string) error {
+	return os.WriteFile(outputPath, []byte(swaggerSpec), 0644)
 }
